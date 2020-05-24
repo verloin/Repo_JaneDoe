932eaa38bba8385302d9aeb0b35aab510a488d00
verloin <ura.assist@gmail.com>
2020-05-24 14:41:43 +0300

    test commit

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,17 @@
-$(function(){
+// $(function(){
 
 
 
-  
+
+// });
+
+document.querySelectorAll('.modalbtn').forEach(function (element) {
+  element.onclick = showModal;
 });
+
+function showModal() {
+  document.querySelector('#modal-wrap').classList.toggle('modal-hide');
+  document.querySelector('body').classList.toggle('backgroundBody');
+
+}
+
